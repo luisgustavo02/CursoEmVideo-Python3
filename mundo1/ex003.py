e42fb99e7027c76174aa1272b798416cc2b1aab1
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2022-08-04 22:17:59 -0300

    Create ex003.py

---
+++ mundo1/ex003.py
@@ -0,0 +1,5 @@
+# CRIE UM PROGRAMA QUE LEIA DOIS NÚMEROS E MOSTRE A SOMA ENTRE ELES
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite um outro número: "))
+print(f"A soma de {num1} e {num2} é {num1+num2}")
