0aacf8eaf0caabc41b950c306e225e712e15edfc
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2022-08-07 16:32:17 -0300

    Update ex005.py

--- mundo1/ex005.py
+++ mundo1/ex005.py
@@ -1,3 +1,5 @@
+# FAÇA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO E MOSTRA NA TELA O SEU SUCESSOR E ANTECESSOR
+
 num = int(input("Digite um número: "))
 
 print(f"O antecessor de {num} é {num-1} e o sucessor de {num} é {num+1}")
