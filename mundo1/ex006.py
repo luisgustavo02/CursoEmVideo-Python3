68848b1f71c0e4294f1b00394fc221292cc44bd6
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2022-08-15 21:55:47 -0300

    Create ex006.py

---
+++ mundo1/ex006.py
@@ -0,0 +1,9 @@
+# CRIE UM ALGORITMO QUE LEIA UM NÚMERO E MOSTRE O SEU DOBRO, TRIPLO E RAIZ QUADRADA
+
+from math import *
+
+num = int(input("DIGITE UM NÚMERO: "))
+
+print(f"O dobro de {num} é {num*2}")
+print(f"O triplo de {num} é {num*3}")
+print(f"A raiz quadrada de {num} é {sqrt(num)}")
