f8a52094870e016499f1b1778ee3aa22d2421363
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2022-10-24 22:46:53 -0300

    Update desafio021.py

--- mundo1/desafio021.py
+++ mundo1/desafio021.py
@@ -1,1 +1,10 @@
+# FAÇA UM PROGRAMA EM PYTHON QUE ABRA E REPRODUZA O ÁUDIO DE UM ARQUIVO MP3.
 
+import pygame
+
+pygame.init()
+
+pygame.mixer.music.load("musica021.mp3")
+pygame.mixer.music.play()
+
+pygame.event.wait()
